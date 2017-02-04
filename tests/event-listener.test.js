99ace8ff8c48828ed63fbbb8266198f88807354e
Vladimir Volchkov <admin@howtoclient.com>
2017-02-04 02:46:32 +0200

    added EventListener structire and basic functionality tests

--- tests/event-listener.test.js
+++ tests/event-listener.test.js
@@ -24,4 +24,37 @@ test('initial tests', () => {
 
 test('test EventListener defenitions', () => {
     const eventListener = TestEvent.addEventListener(noop);
+    expect(eventListener instanceof EventListener).toBe(true);
+    expect(eventListener.remove).not.toEqual(undefined);
+    expect(eventListener.suspend).not.toEqual(undefined);
+    expect(eventListener.restore).not.toEqual(undefined);
+    expect(eventListener.isSuspended).not.toEqual(undefined);
+    expect(eventListener.isRemoved).not.toEqual(undefined);
+});
+
+test('test EventListener functionality', () => {
+    const eventListener = TestEvent.addEventListener(noop);
+    expect(eventListener instanceof EventListener).toBe(true);
+    expect(eventListener.isSuspended()).toEqual(false);
+    expect(eventListener.isRemoved()).toEqual(false);
+
+    eventListener.suspend();
+    expect(eventListener.isSuspended()).toEqual(true);
+    expect(eventListener.isRemoved()).toEqual(false);
+
+    eventListener.restore();
+    expect(eventListener.isSuspended()).toEqual(false);
+    expect(eventListener.isRemoved()).toEqual(false);
+
+    eventListener.remove();
+    expect(eventListener.isSuspended()).toEqual(true);
+    expect(eventListener.isRemoved()).toEqual(true);
+
+    eventListener.suspend();
+    expect(eventListener.isSuspended()).toEqual(true);
+    expect(eventListener.isRemoved()).toEqual(true);
+
+    eventListener.restore();
+    expect(eventListener.isSuspended()).toEqual(true);
+    expect(eventListener.isRemoved()).toEqual(true);
 });
